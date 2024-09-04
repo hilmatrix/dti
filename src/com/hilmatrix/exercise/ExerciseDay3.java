b8a5943ea81a33433802cca792a52652665e4777
Hilmatrix <hilmaul@gmail.com>
2024-09-04 12:08:34 +0700

    added solution for task 2 and 3

--- src/com/hilmatrix/exercise/ExerciseDay3.java
+++ src/com/hilmatrix/exercise/ExerciseDay3.java
@@ -1,5 +1,7 @@
 package com.hilmatrix.exercise;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ExerciseDay3 {
@@ -11,6 +13,7 @@ public class ExerciseDay3 {
             System.out.println("---------------------------");
             System.out.println("Exercise Day 3. Select 0 to exit");
             System.out.println("1) calculate sum digit. 2) check number is prime");
+            System.out.println("3) find largest number. 4) simple calculator");
 
             try {
                 System.out.print("Select task number : ");
@@ -23,7 +26,9 @@ public class ExerciseDay3 {
             scanner = new Scanner(System.in);
 
             switch (taskSelection) {
-                case 1: runTask1(scanner); break;
+                case 1: runTask1countDigit(scanner); break;
+                case 2: runTask2checkIsPrime(scanner); break;
+                case 3: runTask3findLargest(scanner); break;
             }
 
             if (taskSelection == 0)
@@ -31,13 +36,55 @@ public class ExerciseDay3 {
         }
     }
 
-    public static void runTask1(Scanner scanner) {
+    public static void runTask1countDigit(Scanner scanner) {
         System.out.print("Input number : ");
-        String strNumber = scanner.nextLine();
+        int number = scanner.nextInt();
         int count = 0;
-        for (int loop = 0; loop < strNumber.length(); loop++) {
-            count += (int) strNumber.charAt(loop) - (int) '0';
+        while (number > 0) {
+            count += number % 10;
+            number = number / 10;
         }
-        System.out.println("Digit count : " + count);
+        System.out.println("Output : Digit count is " + count);
+    }
+
+    public static void runTask2checkIsPrime(Scanner scanner) {
+        System.out.print("Input number : ");
+        int number = scanner.nextInt();
+        boolean isPrime = true;
+
+        if (number < 2)
+            isPrime = false;
+        else {
+            for (int loop = 2; loop < number; loop++) {
+                if (number % loop == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        String resultStr = " not a prime";
+        if (isPrime)
+            resultStr = " is a prime";
+
+        System.out.println("Output : " + number + resultStr);
+    }
+
+    public static void runTask3findLargest(Scanner scanner) {
+        System.out.print("Input number separated by comma : ");
+
+        String strNumber = scanner.nextLine();
+        String[] strNumberArray = strNumber.replaceAll(" ", "").split(",");
+        int currentValue = Integer.valueOf(strNumberArray[0]);
+
+        if (strNumberArray.length > 1) {
+            for (int loop = 1; loop < strNumberArray.length; loop++) {
+                int nextValue = Integer.valueOf(strNumberArray[loop]);
+                if (currentValue < nextValue)
+                    currentValue = nextValue;
+            }
+        }
+
+        System.out.println("Output : The highest number is " + currentValue);
     }
 }
