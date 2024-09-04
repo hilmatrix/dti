035607d6f5c3b84bc625734991072ae57398af66
Hilmatrix <hilmaul@gmail.com>
2024-09-04 15:21:26 +0700

    add solutions

--- src/com/hilmatrix/exercise/ExerciseDay3.java
+++ src/com/hilmatrix/exercise/ExerciseDay3.java
@@ -14,6 +14,9 @@ public class ExerciseDay3 {
             System.out.println("Exercise Day 3. Select 0 to exit");
             System.out.println("1) calculate sum digit. 2) check number is prime");
             System.out.println("3) find largest number. 4) simple calculator");
+            System.out.println("5) reverse string.      6) Fibonnaci");
+            System.out.println("7) count vowels.        8) bubble sort");
+            System.out.println("9) anagram.             10) second smallest value");
 
             try {
                 System.out.print("Select task number : ");
@@ -29,6 +32,10 @@ public class ExerciseDay3 {
                 case 1: runTask1countDigit(scanner); break;
                 case 2: runTask2checkIsPrime(scanner); break;
                 case 3: runTask3findLargest(scanner); break;
+                case 5: runTask5reverseString(scanner); break;
+                case 6: runTask6Fibonacci(scanner); break;
+                case 7: runTask7vowels(scanner); break;
+                case 8: runTask8bubbleSort(scanner); break;
             }
 
             if (taskSelection == 0)
@@ -55,7 +62,7 @@ public class ExerciseDay3 {
         if (number < 2)
             isPrime = false;
         else {
-            for (int loop = 2; loop < number; loop++) {
+            for (int loop = 2; loop <= Math.sqrt(number); loop++) {
                 if (number % loop == 0) {
                     isPrime = false;
                     break;
@@ -86,5 +93,110 @@ public class ExerciseDay3 {
         }
 
         System.out.println("Output : The highest number is " + currentValue);
+    }
+
+    public static void runTask4simpleCalculator(Scanner scanner) {
+        System.out.print("Input expression : ");
+
+        String strInput = scanner.nextLine().replaceAll(" ","");
+        int operand1 = 0;
+        int operand2 = 0;
+        char operator = '+';
+        String strOperand ="";
+
+        for (int loop = 0; loop < strInput.length(); loop++) {
+            if (Character.isDigit(strInput.charAt(loop))) {
+                strOperand += strInput.charAt(loop);
+            } else if ((strInput.charAt(loop) == '+') || (strInput.charAt(loop) == '-'))  {
+                operand2 = Integer.valueOf(strOperand);
+                //operand1 += operand1
+            }
+        }
+
+    }
+
+    public static void runTask5reverseString(Scanner scanner) {
+        System.out.print("Input string : ");
+
+        String strInput = scanner.nextLine();
+        String strOutput = "";
+        for (int loop = strInput.length()-1; loop >= 0; loop--) {
+            strOutput += strInput.charAt(loop);
+        }
+
+        System.out.println("Output : " + strOutput);
+    }
+
+    public static void runTask6Fibonacci(Scanner scanner) {
+        System.out.print("Input fibonacci value : ");
+        int result;
+
+        int n = scanner.nextInt();
+        int memory1 = 0;
+        int memory2 = 1;
+
+        if ((n < 1) || (n == 1)) {
+            result = memory1;
+        } else if (n == 2) {
+            result = memory2;
+        } else {
+            for (int loop = 3; loop <= n; loop++) {
+                int newMemory = memory1 + memory2;
+                memory1 = memory2;
+                memory2 = newMemory;
+            }
+            result = memory2;
+        }
+
+        System.out.println("Output : " + result);
+    }
+
+    public static void runTask7vowels(Scanner scanner) {
+        System.out.print("Input sentence : ");
+
+        String strInput = scanner.nextLine();
+
+        String vowels = "aiueo";
+        int vowelsCount = 0;
+
+        for (int loop = 0; loop < strInput.length(); loop++) {
+            if (vowels.contains(strInput.charAt(loop)+"")) {
+                vowelsCount++;
+            }
+        }
+
+        System.out.println("Output : " + vowelsCount);
+    }
+
+    public static void runTask8bubbleSort(Scanner scanner) {
+        System.out.print("Input number separated by comma : ");
+
+        String strNumber = scanner.nextLine();
+        String[] strNumberArray = strNumber.replaceAll(" ", "").split(",");
+        List<Integer> integerArray = new ArrayList<Integer>();
+
+        if (strNumberArray.length > 1) {
+            for (int loop = 0; loop < strNumberArray.length; loop++) {
+                integerArray.add(Integer.valueOf(strNumberArray[loop]));
+            }
+        }
+
+        int arrayLength = integerArray.size();
+
+        for (int i = 0; i < arrayLength - 1; i++) {
+            for (int j = 0; j < arrayLength - i - 1; j++) {
+                if (integerArray.get(j) > integerArray.get(j + 1)) {
+                    int temp = integerArray.get(j);
+                    integerArray.set(j,  integerArray.get(j + 1));
+                    integerArray.set(j + 1, temp);
+                }
+            }
+        }
+
+        System.out.println("Output : ");
+        for (int num : integerArray) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
