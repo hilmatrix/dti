0c883b388432702625b421d64de295ee6576b4fd
Hilmatrix <hilmaul@gmail.com>
2024-09-05 10:07:21 +0700

    only provide exercise 3 solutions only

--- src/com/hilmatrix/Main.java
+++ src/com/hilmatrix/Main.java
@@ -5,42 +5,6 @@ import com.hilmatrix.exercise.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int exerciseSelection;
-
-        while (true) {
-            System.out.println("---------------------------");
-            System.out.println("Hilmatrix Exercise Selector");
-            System.out.println("Exercise available : 1-3. Select  0 to exit");
-            System.out.println();
-            try {
-                System.out.print("Select exercise number : ");
-                exerciseSelection = scanner.nextInt();
-            } catch (Exception e) {
-                scanner = new Scanner(System.in);
-                continue;
-            }
-
-            try {
-                switch (exerciseSelection) {
-                    case 1:
-                        ExerciseDay1.run();
-                        break;
-                    case 2:
-                        ExerciseDay2.run();
-                        break;
-                    case 3:
-                        ExerciseDay3.run();
-                        break;
-                }
-            } catch (Exception e) {
-                scanner = new Scanner(System.in);
-                e.printStackTrace();
-                System.out.println("Error at exercise " + exerciseSelection);
-            }
-
-            if (exerciseSelection == 0)
-                break;
-        }
+        ExerciseDay3.run();
     }
 }
