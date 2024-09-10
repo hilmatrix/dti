806b2d15e638fe64b68cc2a6ef03813c7ed19f59
Hilmatrix <hilmaul@gmail.com>
2024-09-10 17:30:22 +0700

    add solution exercise 7 task 2

--- src/com/hilmatrix/exercise/day7/ExerciseDay7.java
+++ src/com/hilmatrix/exercise/day7/ExerciseDay7.java
@@ -1,5 +1,10 @@
 package com.hilmatrix.exercise.day7;
 
+import javax.xml.crypto.Data;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,6 +18,7 @@ public class ExerciseDay7 {
             System.out.println("---------------------------");
             System.out.println("Exercise Day 7. Select 0 to exit");
             System.out.println("Task 1 calculate average");
+            System.out.println("Task 2 calculate summary");
 
             try {
                 System.out.print("Select task number : ");
@@ -26,6 +32,7 @@ public class ExerciseDay7 {
 
             switch (taskSelection) {
                 case 1 : runTask1calculateAverage(scanner); break;
+                case 2 : runTask2dataSummary(); break;
             }
 
             if (taskSelection == 0)
@@ -61,5 +68,98 @@ public class ExerciseDay7 {
             average += number;
         }
         System.out.println("Average = " + average);
+    }
+
+    public static void runTask2dataSummary() {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream inputStream = null;
+        BufferedReader reader = null;
+
+        try  {
+            inputStream = classLoader.getResourceAsStream("product_sales_data.csv");
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            if (inputStream == null) {
+                System.out.println("Resource not found.");
+                return;
+            }
+
+            DataParser newData = new DataParser();
+
+            String line = reader.readLine();
+            line = reader.readLine();
+            newData.parse(line);
+
+            int lineCount = 1;
+            double totalSales = newData.sold * newData.price;
+            int productSold = newData.sold;
+
+            DataParser mostSoldProduct = new DataParser(newData);
+            DataParser leastSoldProduct = new DataParser(newData);
+
+            while ((line = reader.readLine()) != null) {
+                try {
+                    newData.parse(line);
+                    totalSales += newData.sold * newData.price;
+                    productSold += newData.sold;
+                    if (newData.sold < leastSoldProduct.sold) {
+                        leastSoldProduct.copy(newData);
+                    }
+                    if (newData.sold > mostSoldProduct.sold) {
+                        mostSoldProduct.copy(newData);
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+
+            System.out.println("Total sales = " + totalSales);
+            System.out.println("Total product sold = " + productSold);
+            System.out.println("Most Bought Product = " + mostSoldProduct.product);
+            System.out.println("Least Bought Product = " + leastSoldProduct.product);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Manually close the resources
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
+    public static class DataParser {
+        String product;
+        int sold;
+        double price;
+
+        public DataParser() {}
+
+        public DataParser(DataParser newData) {
+            copy(newData);
+        }
+
+        public void parse(String data) {
+            String[] parts = data.split(",");
+
+            product = parts[0]; // "product"
+            sold = Integer.parseInt(parts[1]); // 10
+            price = Float.parseFloat(parts[2]); // 50.5
+        }
+
+        public void copy(DataParser newData) {
+            this.product = newData.product;
+            this.sold = newData.sold;
+            this.price = newData.price;
+        }
     }
 }
